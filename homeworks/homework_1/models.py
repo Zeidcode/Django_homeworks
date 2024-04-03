04ef330a0a427a59f502ccf11f3d239dea110771
ZeidCode <zeidcode@gmail.com>
2024-04-03 09:08:49 +0400

    + models

--- homeworks/homework_1/models.py
+++ homeworks/homework_1/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    address = models.CharField(max_length=255)
+    registration_date = models.DateTimeField(default=timezone.now)
+
+    def str(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    added_date = models.DateTimeField(default=timezone.now)
+
+    def str(self):
+        return self.name
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    order_date = models.DateTimeField(default=timezone.now)
+
+    def str(self):
+        return f"Order #{self.pk} by {self.client.name}"
