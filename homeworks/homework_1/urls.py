96724d1f77a381225105a825b1b0536db2ace53c
ZeidCode <zeidcode@gmail.com>
2024-04-05 11:03:04 +0400

    +order sorting by timeline

--- homeworks/homework_1/urls.py
+++ homeworks/homework_1/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name="main"),
-    path('about', views.about, name="about"),
+    path('', views.index, name='index'),
+    path('clients/', views.client_list, name='client_list'),
+    path('products/', views.product_list, name='product_list'),
+    path('orders/', views.order_list, name='order_list'),
+    path('ordered_products/', views.ordered_products, name='ordered_products'),
 ]
--- homeworks/homework_1/views.py
+++ homeworks/homework_1/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def main(request):
-    return render(request, "index.html")
+from .models import Client, Product, Order
+from django.utils import timezone
+from datetime import timedelta
+from .models import Order, Product
 
-def about(request):
-    return render(request, "about_me.html")
+def index(request):
+    return render(request, 'index.html')
+
+def client_list(request):
+    clients = Client.objects.all()
+    return render(request, 'clients.html', {'clients': clients})
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'products.html', {'products': products})
+
+def order_list(request):
+    orders = Order.objects.all()
+    return render(request, 'orders.html', {'orders': orders})
+
+def ordered_products(request):
+    # Определяем временные интервалы для последних 7, 30 и 365 дней
+    intervals = {
+        'week': timezone.now() - timedelta(days=7),
+        'month': timezone.now() - timedelta(days=30),
+        'year': timezone.now() - timedelta(days=365)
+    }
+    
+    # Получаем заказанные товары за каждый из интервалов
+    ordered_products = {}
+    for interval, start_date in intervals.items():
+        orders = Order.objects.filter(order_date__gte=start_date)
+        products = Product.objects.filter(order__in=orders).distinct()
+        ordered_products[interval] = products
+
+    return render(request, 'ordered_products.html', {'ordered_products': ordered_products})
